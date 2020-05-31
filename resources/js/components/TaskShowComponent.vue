2a890121ab57ca061f605eb08800ed156cfa72f0
宮本光直 <mitu3270uw@yahoo.co.jp>
2020-05-31 21:41:21 +0900

    fix_各コンポーネント修正

--- resources/js/components/TaskShowComponent.vue
+++ resources/js/components/TaskShowComponent.vue
@@ -6,22 +6,27 @@
                     <div class="form-group row border-bottom">
                         <label for="id" class="col-sm-3 col-form-label">ID</label>
                         <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id"
-                            v-bind:value="taskId">
+                            v-model="task.id">
                     </div>
                     <div class="form-group row border-bottom">
                         <label for="title" class="col-sm-3 col-form-label">Title</label>
                         <input type="text" class="col-sm-9 form-control-plaintext" readonly id="title"
-                            value="title title">
+                            v-model="task.title">
                     </div>
                     <div class="form-group row border-bottom">
                         <label for="content" class="col-sm-3 col-form-label">Content</label>
                         <input type="text" class="col-sm-9 form-control-plaintext" readonly id="content"
-                            value="content content">
+                            v-model="task.content">
                     </div>
                     <div class="form-group row border-bottom">
                         <label for="person-in-charge" class="col-sm-3 col-form-label">Person In Charge</label>
                         <input type="text" class="col-sm-9 form-control-plaintext" readonly id="person-in-charge"
-                            value="Ichiro">
+                            v-model="task.person_in_charge">
+                    </div>
+                    <div class="form-group row border-bottom">
+                        <label for="person-in-charge" class="col-sm-3 col-form-label">updated_time</label>
+                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="person-in-charge"
+                            v-model="task.updated_at">
                     </div>
                 </form>
             </div>
@@ -30,9 +35,27 @@
 </template>
 
 <script>
+
     export default {
         props: {
             taskId: String
+        },
+        data: function () {
+            return {
+                task: []
+            }
+        },
+        methods: {
+            getTaskById() {
+                console.log(this.taskId);
+                axios.get('/api/tasks/' + this.taskId)
+                    .then((res) => {
+                        this.task = res.data;
+                    });
+            }
+        },
+        mounted() {
+            this.getTaskById();
         }
     }
 
