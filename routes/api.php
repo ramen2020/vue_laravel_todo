e6d289333bb45b2ef1c78585090225ba61bfb1c5
宮本光直 <mitu3270uw@yahoo.co.jp>
2020-05-31 21:39:05 +0900

    add_ルーティングにstore追加

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/tasks', 'TaskController@index');
+Route::post('/tasks', 'TaskController@store');
 Route::get('/tasks/{task}', 'TaskController@show');
 Route::put('/tasks/{task}', 'TaskController@update');
 Route::delete('/tasks/{task}', 'TaskController@destroy');
